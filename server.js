a34f10ce1b4496606adf8fe8b783c4e55de16460
vilas-z <vasantzugare1978@gmail.com>
2025-10-03 16:53:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const DATA_FILE = path.join(__dirname, 'data.json');
 
@@ -102,4 +102,5 @@ app.post('/api/setup-match', (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
+
 });
